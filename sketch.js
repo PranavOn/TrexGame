5ba57afe107e1651d8443fa00cdc4564bb307832
PranavOn <93942231+PranavOn@users.noreply.github.com>
2021-11-17 18:57:06 +0000

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,97 +1,55 @@
-var b, enemyImage, ballImage, scene, ball, alien1, alien2, alien3, score=0, opscore=0, speed=5
+var trex, trex_running;
+var ground, ground_image;
+var cactus;
+var invisibleGround;
 
-function preload(){
-  
-  b = loadImage("b.png");
-  enemyImage = loadImage("alien.png");
-  ballImage = loadImage("ball.png");
-  
+function preload() {
+  trex_running = loadAnimation("trex1.png", "trex3.png", "trex4.png");
+  ground_image = loadImage("ground2.png");
 }
 
-
-
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 200);
+
+  //create a trex sprite
+  trex = createSprite(50, 170, 20, 90);
+
+  invisibleGround = createSprite(200, 190, 400, 20);
+
+  trex.addAnimation("running", trex_running);
+  trex.scale = 0.5;
   
-  scene = createSprite(200,200);
-  scene.addImage(b)
-  ball = createSprite(200,345,200,345)
-  ball.addImage(ballImage)
-  ball.scale=0.1
-
-  alien1 = createSprite(200,280,10,10)
-  alien2 = createSprite(200,170,10,10)
-  alien3 = createSprite(200,50,10,10)
-  alien1.addImage(enemyImage)
-  alien2.addImage(enemyImage)
-  alien3.addImage(enemyImage)
-
-  alien1.scale=0.3
-  alien2.scale=0.3
-  alien3.scale=0.3
-
-
-  
-   score = 0    
+  // ground sprite () -> so we need to move it backwards.
+  ground = createSprite(200, 180, 400, 20);
+  ground.addImage(ground_image);
+  ground.x = ground.width / 2;
+  invisibleGround.visible = false;
+  // Ground velocities
+  ground.velocityX = -3;
 }
 
 function draw() {
- background(0);
- createEdgeSprites()
+  background("black");
+  drawSprites();
 
- alien1.x=alien1.x+speed
- alien2.x=alien2.x-speed
- alien3.x=alien3.x+speed
+  // Keyboard Input
+  if (keyDown("space") && trex.y > 100) {
+    trex.velocityY = -10;
+  }
 
- if(alien1.x<0 || alien1.x>width)
- {
-   speed=speed*-1
- }
+  // Trex Movement
+  trex.velocityY += 0.5;
 
+  /**
+   * If the ground goes out of the screen,
+   * this function will reset it to the
+   * screen.
+   */
+  if (ground.x < 0) {
+    ground.x = ground.width / 2;
+  }
 
+  trex.collide(invisibleGround);
 
-if(keyDown(UP_ARROW)){
-  ball.y=ball.y-3
+  console.log(ground.x);
 }
-
-if(keyDown(DOWN_ARROW)){
-  ball.y=ball.y+3
-}
-
-if(keyDown(LEFT_ARROW)){
-  ball.x=ball.x-3
-}
-
-if(keyDown(RIGHT_ARROW)){
-  ball.x=ball.x+3
-}
-
-if(ball.isTouching(alien1)|| ball.isTouching(alien2)|| ball.isTouching(alien3)){
-    ball.x=200
-  ball.y=350
-  opscore = opscore+1
-}
-if(ball.y<0){
-  ball.x=200
-  ball.y=345
-  score=score+1
-}
-
-
-drawSprites();
-textSize(15)
-  fill("Yellow")
-  text("Humans:"+score,328,30);
-  
-
-textSize(15)
-  fill("Yellow")
-  text("Aliens:"+opscore,5,30);
-  
-
-    
-
-}
-
-
-
